9215581b461245c5d5f23a303c8182ecde715f2e
gwildar <gwilym.johnston@isotoma.com>
2016-07-20 11:37:14 +0100

    adding missing loaders and adding webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin'),
-StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin'),
-locals = {
-  routes: [
-  '/',
-  ]
-};
+    webpack = require('webpack');
+    StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin'),
+    locals = {
+      routes: [
+      '/',
+      ]
+    };
 
 module.exports = {
-  entry: './src/index',
+  entry: [
+      'webpack/hot/only-dev-server',
+      './src/index'
+    ],
   devtool: 'source-map',
   output: {
     path: './build',
@@ -17,24 +21,40 @@ module.exports = {
   },
   module: {
     loaders: [
-    {
-      test: /\.js$/,
-      loader: 'babel',
+    { 
+      test: /\.js?$/, 
+      loaders: ['react-hot', 'babel'], 
       include: __dirname + '/src',
+      exclude: /node_modules/ 
+    },
+    { 
+      test: /\.js$/, 
+      exclude: /node_modules/, 
+      loader: 'babel-loader'
     },
     {
       test: /\.less$/,
       loader: ExtractTextPlugin.extract('css?sourceMap!less?sourceMap')
     },
-    { test: /\.(woff|woff2)$/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-    { test: /\.ttf$/,    loader: "file-loader" },
-    { test: /\.eot$/,    loader: "file-loader" },
-    { test: /\.svg$/,    loader: "file-loader" }
+    { 
+      test: /\.(woff|woff2)$/,  
+      loader: "url-loader?limit=10000&mimetype=application/font-woff" 
+    },
+    { test: /\.ttf$/,    
+      loader: "file-loader" 
+    },
+    { test: /\.eot$/,    
+      loader: "file-loader" 
+    },
+    { test: /\.svg$/,    
+      loader: "file-loader" 
+    }
     ],
   },
   plugins: [
     new ExtractTextPlugin("styles.css"),
-    new StaticSiteGeneratorPlugin('main', locals.routes),
+    new webpack.NoErrorsPlugin(),
+    new StaticSiteGeneratorPlugin('main', locals.routes)
   ],
   resolve: {
     modulesDirectories: ['node_modules'],
