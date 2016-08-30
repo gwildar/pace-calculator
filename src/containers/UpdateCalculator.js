4d7a762afeaa7867b6874895c5d39cfa45742097
gwilym <gwilym.johnston@isotoma.com>
2016-08-30 18:06:45 +0100

    towards updateCalculator container

--- src/containers/UpdateCalculator.js
+++ src/containers/UpdateCalculator.js
@@ -2,3 +2,27 @@ import { connect } from 'react-redux';
 import actions from '../actions';
 
 import PaceCalculator from '../components/PaceCalculator';
+
+
+const mapStateToProps = (state) => {
+  return {
+    time: "1",
+    distance: "2",
+    pace: "3",
+  }
+}
+
+const mapDispatchToProps = (Dispatch) => {
+  return {
+    distanceChange : (distance) => {
+      dispatch(updateDistance(distance))
+    }
+  }
+}
+
+const UpdateCalculator = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PaceCalculator)
+
+export default UpdateCalculator;
